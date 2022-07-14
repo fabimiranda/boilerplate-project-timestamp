492bb5aaaaffe6e5c78074b2b4053ae6202374d3
fabimiranda <fabianamgaray@gmail.com>
2022-07-14 17:30:52 -0400

    hello

--- index.js
+++ index.js
@@ -20,11 +20,23 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:timestamp", function (req, res) {
+  let timestamp = req.params.timestamp;
+  if(!timestamp.match(/\d{5,}/)){
+    timestamp = +timestamp;
+  }
+  let date = new Date(timestamp);
+if(date.toUTCString() == "Invalid Date"){
+  res.json({error: date.toUTCString()})
+}
+  res.json({ unix: date.valueOf(), utc: date.toUTCString()});
 });
 
-
+app.get("/api/timestamp/", (req, res) => { 
+  let date = new Date();
+  res.json({ unix: date.valueOf(), utc: date.toUTCString()});
+}
+)
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
